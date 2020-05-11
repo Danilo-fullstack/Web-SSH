b71e060a48fcda9e41c78d006ebe5afcdd542d31
Danilo-fullstack <danilosantosdeoliveira40@gmail.com>
2020-05-11 15:04:17 -0300

    update

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     define('HOST', '127.0.0.1');
     define('USER', 'root');
     define('SENHA', '');
-    define('DB', 'daniloweb');
+    define('DB', 'webssh');
 
     $conexao = mysqli_connect(HOST, USER, SENHA, DB) or die ('Não foi possível conectar');
 ?>
